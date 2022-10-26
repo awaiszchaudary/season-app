793392a3ea32487fa58b4779d5db8e018125f6c2
unknown <awaiszchaudary@gmail.com>
2022-10-26 11:49:05 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import  {View} from 'react-native';
+
+import {Text} from 'react-native-elements';
+
+import Stack from './src/Stack/Stack';
+
+const App = () => {
+  return (
+    <Stack />
+  );
+}
+
+export default App;
